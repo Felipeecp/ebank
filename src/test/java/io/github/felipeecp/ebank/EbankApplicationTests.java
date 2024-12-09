5ece4abee26eef14cd58d27672b6abe5d0d14303
Felipeecp <felipe.engcp@gmail.com>
2024-12-09 14:01:37 -0300

    inital commit

---
+++ src/test/java/io/github/felipeecp/ebank/EbankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipeecp.ebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
